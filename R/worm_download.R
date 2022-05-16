d16538e30546cd4ef9f15e1a8b3a1336f11071ba
yamaken37 <samadakenji@gmail.com>
2022-05-16 07:35:45 +0000

    update gitignore

---
+++ R/worm_download.R
@@ -0,0 +1,17 @@
+worm_download <- function(dist="mSBD"){
+    Ds <- NULL
+    tf <- tempfile()
+    if(dist == "mSBD"){
+        download.file("https://figshare.com/ndownloader/files/34737937", tf)
+    }else if(dist == "Euclid"){
+        download.file("https://figshare.com/ndownloader/files/34737931", tf)
+    }else{
+        stop("Please specify dist as 'mSBD' or 'Euclid'!")
+    }
+    load(tf)
+    Ds
+}
+
+# 使い方
+Ds <- worm_download("mSBD")
+Ds <- worm_download("Euclid")
