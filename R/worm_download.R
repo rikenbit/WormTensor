818dd661d0f6d4c96d2f9cf1e20735bb963772ee
yamaken37 <samadakenji@gmail.com>
2022-05-23 02:17:08 +0000

    add comment worm_download

--- R/worm_download.R
+++ R/worm_download.R
@@ -1,3 +1,11 @@
+#' Downloads distance matrices
+#' 28 animals' data including 24 normal and 4 noisy are retrieved from at figshare.
+#' @param dist # 追記する
+#'
+#' @return
+#' @export
+#'
+#' @examples
 worm_download <- function(dist="mSBD"){
     Ds <- NULL
     tf <- tempfile()
